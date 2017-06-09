a7c2d448244691c9eb068269da54085e248fef08
clareon <clareon@gmail.com>
2017-06-10 00:12:50 +0300

    First commit

---
+++ src/First.java
@@ -0,0 +1,13 @@
+public class First {
+
+    public static void main(String[] args) {
+       MustangLogic m = new MustangLogic();
+        m.JumpMustang();
+    }
+}
+
+class MustangLogic {
+    public void JumpMustang(){
+        System.out.println("Hello World teatcher!");
+    }
+}

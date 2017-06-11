57fc0b2423704b56d1e3541aab2bb311ed9f2566
clareon <clareon@gmail.com>
2017-06-12 00:55:19 +0300

    First commit

---
+++ src/chapt01/Hello.java
@@ -0,0 +1,26 @@
+package chapt01;
+
+public class Hello
+{
+    public static void main(){
+        System.out.print("A");
+    }
+    public static void main(String args){
+        System.out.print("B");
+    }
+
+    public static void main(String[] args){
+        System.out.println("C");
+    }}
+
+   /*public static void main(String[] args) {
+       MustangLogic m = new MustangLogic();
+        m.JumpMustang();
+    }
+}
+
+class MustangLogic {
+    public void JumpMustang(){
+        System.out.println("Hello World teatcher branch Alex 3 commit!");
+    }
+}*/

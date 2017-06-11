e36cb6c54a6c9a26507c20415428b9a451ed805e
clareon <clareon@gmail.com>
2017-06-12 01:02:53 +0300

    First commit

--- src/chapt01/Hello.java
+++ src/chapt01/Hello.java
@@ -2,16 +2,11 @@ package chapt01;
 
 public class Hello
 {
-    public static void main(){
-        System.out.print("A");
-    }
-    public static void main(String args){
-        System.out.print("B");
-    }
 
     public static void main(String[] args){
-        System.out.println("C");
-    }}
+        System.out.println("Hello users!");
+    }
+}
 
    /*public static void main(String[] args) {
        MustangLogic m = new MustangLogic();

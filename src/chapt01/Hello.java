8307c90e1295d93e7389bdd3c32b63be10107680
clareon <clareon@gmail.com>
2017-06-15 01:03:36 +0300

    First commit

--- src/chapt01/Hello.java
+++ src/chapt01/Hello.java
@@ -1,14 +1,34 @@
 package chapt01;
 
+import jdk.internal.org.objectweb.asm.tree.TryCatchBlockNode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Calendar;
 
 public class Hello
 {
 
     public static void main(String[] args){
+        String Passvd = "user";
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        try{
+            System.out.println("Введите пароль");
+            String userPasss = br.readLine();
+            if (Passvd.equals(userPasss) ) System.out.println("Пароли совпадают");
+            else System.out.println("Пароли не совпадают");
+        }
+
+        catch(IOException e){
+            System.err.print("Ошибка ввода" + e);
+        }
+
+ /*S
         for (String str: args)
         System.out.printf("Args = %s", str);
-        /*System.out.println("Hello World teatcher branch Alex 3 commit!" +Calendar.getInstance().getTimeZone() );*/
+       ystem.out.println("Hello World teatcher branch Alex 3 commit!" +Calendar.getInstance().getTimeZone() );*/
     }
 }
 

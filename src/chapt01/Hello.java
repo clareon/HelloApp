b3b563389efb449268093259aa9aff6d79b5518b
clareon <clareon@gmail.com>
2017-06-15 01:12:35 +0300

    First commit

--- src/chapt01/Hello.java
+++ src/chapt01/Hello.java
@@ -2,10 +2,8 @@ package chapt01;
 
 import jdk.internal.org.objectweb.asm.tree.TryCatchBlockNode;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Calendar;
+import java.io.*;
+
 
 public class Hello
 {

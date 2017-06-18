ac7b24b3fd4ed1897e0576c99c2a13917e209754
clareon <clareon@gmail.com>
2017-06-18 19:58:36 +0300

    First commit

--- src/chapt01/Hello.java
+++ src/chapt01/Hello.java
@@ -1,6 +1,5 @@
 package chapt01;
 
-import jdk.internal.org.objectweb.asm.tree.TryCatchBlockNode;
 
 import java.io.*;
 
@@ -11,16 +10,17 @@ public class Hello
     public static void main(String[] args){
         /*String Passvd = "user";
         InputStreamReader isr = new InputStreamReader(System.in);
-        BufferedReader br = new BufferedReader(isr);
+        BufferedReader br = new BufferedReader(isr);*/
 
-        try {*/
-            System.out.println("Введите пароль");
+        try {
+            System.out.println("Enter pass");
+
            /* String userPasss = br.readLine();
             if (Passvd.equals(userPasss) ) System.out.println("Пароли совпадают");
             else System.out.println("Пароли не совпадают");
-        }
-        catch (IOException e){
-            System.err.print("Ошибка ввода" + e);
+        */}
+        finally {
+            System.err.println("Error input");
         }
 
 
